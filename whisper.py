e624bd982b698500607d51b1deec92986472e632
Seymur <42584517+seymurk95@users.noreply.github.com>
2025-11-28 19:53:18 +0400

    Add files via upload

---
+++ whisper.py
@@ -0,0 +1,24 @@
+from transformers import pipeline
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model="openai/whisper-medium",
+)
+
+# Укажи путь к твоему файлу
+audio_file = r"C:\pyton\projects\NVIDEA_NN\Zadachi.wav"
+
+# Распознаём речь
+result = pipe(audio_file, language="ru", task="transcribe")
+
+# Текст из модели
+text = result["text"]
+
+# Выводим в терминал
+print("TEXT:", text)
+
+# Записываем в файл
+with open("result.txt", "w", encoding="utf-8") as f:
+    f.write(text)
+
+print("Готово! Текст сохранён в result.txt")
