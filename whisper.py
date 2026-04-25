80555b29907a54bb6efdd31c823e353e8105f264
Seymur <42584517+seymurk95@users.noreply.github.com>
2026-04-25 19:45:18 +0400

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -1,37 +1,32 @@
 import torch
 import librosa
+import gc
+import os
+from transformers import pipeline
 import sys
-from transformers import pipeline
 
-# Определение устройства
+# === Оптимизация памяти ===
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+torch.cuda.empty_cache()
+gc.collect()
+
+# Получаем путь к аудио
+if len(sys.argv) > 1:
+    audio_file = sys.argv[1]
+else:
+    audio_file = "nepr.wav"
+
+# Определяем устройство
 device = 0 if torch.cuda.is_available() else -1
+print(f"Whisper запущен на: {'GPU' if device == 0 else 'CPU'}")
 
-# Инициализируем пайплайн
+# Загружаем модель
 pipe = pipeline(
     "automatic-speech-recognition",
     model="openai/whisper-medium",
-    torch_dtype=torch.float16 if device == 0 else torch.float32,
-    device=device
-)import torch
-import librosa
-from transformers import pipeline
-import sys # Добавляем для чтения аргументов
-
-# Проверка наличия аргумента
-if len(sys.argv) < 2:
-    print("Ошибка: Не указан путь к аудиофайлу.")
-    print("Использование: python whisper.py <путь_к_файлу>")
-    sys.exit(1)
-
-
-
-device = 0 if torch.cuda.is_available() else -1
-
-pipe = pipeline(
-    "automatic-speech-recognition",
-    model="openai/whisper-medium",
-    torch_dtype=torch.float16 if device == 0 else torch.float32,
-    device=device
+    dtype=torch.float16 if device == 0 else torch.float32,
+    device=device,
+    model_kwargs={"attn_implementation": "sdpa"}
 )
 
 print(f"Загрузка аудио: {audio_file}...")
@@ -43,7 +38,7 @@ result = pipe(
     chunk_length_s=30,
     stride_length_s=(5, 5),
     batch_size=8,
-    return_timestamps=True,
+    return_timestamps=False,
     generate_kwargs={"language": "russian", "task": "transcribe"}
 )
 
@@ -51,35 +46,5 @@ text = result["text"]
 
 with open("raw_text.txt", "w", encoding="utf-8") as f:
     f.write(text)
-print("Транскрипция сохранена в raw_text.txt")
 
-if len(sys.argv) > 1:
-    audio_file = sys.argv[1]
-else:
-    # Оставляем значение по умолчанию, чтобы файл работал сам по себе
-    audio_file = "Rastvori.wav"
-
-print("Загрузка аудио...")
-# Загружаем
-audio_array, sampling_rate = librosa.load(audio_file, sr=16000)
-
-print("Распознавание речи с перекрытием (stride)...")
-
-
-result = pipe(
-    audio_array,
-    chunk_length_s=30,      # Длина куска
-    stride_length_s=(5, 5), # ВАЖНО: Делаем перекрытие по 5 секунд с краев
-    batch_size=8,           # Обработка пачками
-    return_timestamps=True, # Помогает модели не терять нить повествования
-    generate_kwargs={"language": "russian", "task": "transcribe"}
-)
-
-text = result["text"]
-print("\nмодель Wisper завершила свою работу")
-#print(text)
-
-with open("raw_text.txt", "w", encoding="utf-8") as f:
-    f.write(text)
-
-
+print("✅ Транскрипция сохранена в raw_text.txt")
